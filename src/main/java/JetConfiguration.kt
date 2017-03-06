99ad4c413a3b07a17e652e58573cdc219ba3a637
Codetector <codetector@codetector.cn>
2017-03-06 19:34:16 +0800

    Configuration Interface

---
+++ src/main/java/JetConfiguration.kt
@@ -0,0 +1,27 @@
+/**
+ * Created by Codetector on 2017/3/6.
+ * Project Classroom
+ */
+interface JetConfiguration {
+    fun get(key: String, defaultValue: Any = Any()):Any
+
+    fun getString(key: String, defaultValue: String = String()):String
+    fun set(key:String, value: String)
+
+    fun getShort(key: String, defaultValue: Short): Short
+    fun set(key: String, value: Short)
+
+    fun getInteger(key: String, defaultValue: Int): Int
+    fun set(key: String, value: Int)
+
+    fun getFloat(key: String, defaultValue: Float): Float
+    fun set(key: String, value: Float)
+
+    fun getDouble(key: String, defaultValue: Double): Double
+    fun set(key: String, value: Double)
+
+    fun getBoolean(key: String, defaultValue: Boolean): Boolean
+    fun set(key: String, value: Boolean)
+
+    fun getCollection()
+}
